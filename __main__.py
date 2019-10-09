65674a2238de14fa5c3481c33893768c5307b3ca
Metehan Ozten <m_ozten@yahoo.com>
2019-10-09 10:41:27 -0700

    Personalizing UPNP

--- __main__.py
+++ __main__.py
@@ -11,35 +11,24 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-def setup_debugging():
-    """
-    Load PyCharm's egg and start a remote debug session.
-    :return: None
-    """
-    import sys
-    sys.path.append('/root/pycharm-debug-py3k.egg')
-    import pydevd
-    pydevd.settrace('192.168.4.47', port=5422, stdoutToServer=True, stderrToServer=True, suspend=False)
-
-
-setup_debugging()
-
-
-def get_network_interface_ip_address(interface='eth0'):
+def get_network_interface_ip_address():
     """
     Get the first IP address of a network interface.
     :param interface: The name of the interface.
     :return: The IP address.
     """
     while True:
-        if NETWORK_INTERFACE not in ni.interfaces():
-            logger.error('Could not find interface %s.' % (interface,))
-            exit(1)
+        network_interface = None
+        for potential_network_interface in ni.interfaces():
+            if potential_network_interface != 'lo' and potential_network_interface != 'enp1s0' and potential_network_interface != 'enp2s0':
+                network_interface = potential_network_interface
+            else:
+                pass
+        if network_interface is None:
+            return ""
         interface = ni.ifaddresses(interface)
         if (2 not in interface) or (len(interface[2]) == 0):
-            logger.warning('Could not find IP of interface %s. Sleeping.' % (interface,))
-            sleep(60)
-            continue
+            return ""
         return interface[2][0]['addr']
 
 
@@ -47,14 +36,14 @@ device_uuid = uuid.uuid4()
 local_ip_address = get_network_interface_ip_address(NETWORK_INTERFACE)
 
 http_server = UPNPHTTPServer(8088,
-                             friendly_name="Jambon 3000",
-                             manufacturer="Boucherie numérique SAS",
-                             manufacturer_url='http://www.boucherie.example.com/',
-                             model_description='Jambon Appliance 3000',
-                             model_name="Jambon",
-                             model_number="3000",
-                             model_url="http://www.boucherie.example.com/en/prducts/jambon-3000/",
-                             serial_number="JBN425133",
+                             friendly_name="NXE 400",
+                             manufacturer="Nexa3D",
+                             manufacturer_url='https://nexa3d.com/',
+                             model_description='NXE 400 3D Printer',
+                             model_name="NXE",
+                             model_number="400",
+                             model_url="https://nexa3d.com/3d-printers/",
+                             serial_number="1234",
                              uuid=device_uuid,
                              presentation_url="http://{}:5000/".format(local_ip_address))
 http_server.start()
@@ -63,5 +52,5 @@ ssdp = SSDPServer()
 ssdp.register('local',
               'uuid:{}::upnp:rootdevice'.format(device_uuid),
               'upnp:rootdevice',
-              'http://{}:8088/jambon-3000.xml'.format(local_ip_address))
+              'http://{}:8088/nxe-400.xml'.format(local_ip_address))
 ssdp.run()
